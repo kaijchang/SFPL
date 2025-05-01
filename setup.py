9f649e537fdb86f6d30b072a2d529fa3dd95c23b
Kai Chang <kaijchang@gmail.com>
2025-04-30 23:18:48 -0400

    v1.5.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 setup(
     name='sfpl',
     packages=['sfpl'],
-    version='1.5.6',
+    version='1.5.7',
     description='Unofficial Python API for SFPL',
     author='Kai Chang',
     author_email='kaijchang@gmail.com',
@@ -17,11 +17,4 @@ setup(
         os.path.dirname(__file__)), 'README.md')).read(),
     install_requires=[open(os.path.join(os.path.abspath(
         os.path.dirname(__file__)), 'requirements.txt')).read().split('\n')[:-1]],
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-    ]
 )
