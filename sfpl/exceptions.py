3ac102c1c072c344cb07d1948be3c314fd7abd82
Lukas Schwab <lukas.schwab@gmail.com>
2025-05-05 16:17:48 -0700

    Totally optional: run `ruff format .`

--- sfpl/exceptions.py
+++ sfpl/exceptions.py
@@ -7,14 +7,14 @@ class NotOnHold(Exception):
     """Raised when a user tries to cancel a hold on a book they aren't holding."""
 
     def __init__(self, book):
-        Exception.__init__(self, '{} is not on hold.'.format(book))
+        Exception.__init__(self, "{} is not on hold.".format(book))
 
 
 class NotCheckedOut(Exception):
     """Raised when a user tries to renew a book they haven't checked out."""
 
     def __init__(self, book):
-        Exception.__init__(self, '{} is not checked out.'.format(book))
+        Exception.__init__(self, "{} is not checked out.".format(book))
 
 
 class InvalidSearchType(Exception):
@@ -22,21 +22,25 @@ class InvalidSearchType(Exception):
 
     def __init__(self, _type):
         Exception.__init__(
-            self, "{} is not a valid search type. Valid search types are 'keyword', 'title', 'author', 'subject', 'tag' and 'list'.".format(_type))
+            self,
+            "{} is not a valid search type. Valid search types are 'keyword', 'title', 'author', 'subject', 'tag' and 'list'.".format(
+                _type
+            ),
+        )
 
 
 class NoBranchFound(Exception):
     """Raised when no matches are found for a user's branch search."""
 
     def __init__(self, branch):
-        Exception.__init__(self, 'No matches found for {}.'.format(branch))
+        Exception.__init__(self, "No matches found for {}.".format(branch))
 
 
 class NoUserFound(Exception):
     """Raised when no matches are found for a user's user search."""
 
     def __init__(self, user):
-        Exception.__init__(self, 'No match found for {}'.format(user))
+        Exception.__init__(self, "No match found for {}".format(user))
 
 
 class LoginError(Exception):
@@ -64,10 +68,13 @@ class MissingFilterTerm(Exception):
     """Raised when a search term doesn't have include or exclude in it."""
 
     def __init__(self):
-        Exception.__init__(self,
-            "Each search term needs to include 'exclude' or 'include' and a valid term type such as 'keyword', 'author', 'title', 'subject', 'series', 'award', 'identifier', 'region', 'genre', 'publisher' or 'callnumber'")
+        Exception.__init__(
+            self,
+            "Each search term needs to include 'exclude' or 'include' and a valid term type such as 'keyword', 'author', 'title', 'subject', 'series', 'award', 'identifier', 'region', 'genre', 'publisher' or 'callnumber'",
+        )
 
 
 class NotLoggedIn(Exception):
     """Raised when an authentication token is rejected."""
+
     pass
--- tests/test_api.py
+++ tests/test_api.py
@@ -43,15 +43,15 @@ class TestScraper(unittest.TestCase):
         self.assertEqual(result[0]._id, "S93C6223776")
 
     def test_author_search(self):
-        author = sfpl.Search('J.K. Rowling', _type='author')
+        author = sfpl.Search("J.K. Rowling", _type="author")
         results = author.getResults()
 
         for result in next(results):
-            self.assertTrue('Rowling, J. K' in result.author)
+            self.assertTrue("Rowling, J. K" in result.author)
 
     def test_pagination(self):
-        'test pagination'
-        author = sfpl.Search('J.K. Rowling', _type='author')
+        "test pagination"
+        author = sfpl.Search("J.K. Rowling", _type="author")
         results = author.getResults(pages=2)
 
         page_one = next(results)
@@ -59,33 +59,33 @@ class TestScraper(unittest.TestCase):
         self.assertNotEqual(page_one, page_two)
 
     def test_book_search(self):
-        search = sfpl.Search('Python')
+        search = sfpl.Search("Python")
 
         results = search.getResults()
 
         for result in next(results):
-            self.assertTrue('python' in result.title.lower())
+            self.assertTrue("python" in result.title.lower())
 
     def test_book_search_with_zero_results(self):
-        'test book search with zero results'
-        search = sfpl.Search('qwteyut_does_not_exist')
+        "test book search with zero results"
+        search = sfpl.Search("qwteyut_does_not_exist")
         self.assertRaises(RuntimeError, lambda: next(search.getResults()))
 
     def test_book_search_with_one_result(self):
-        'test book search with one result'
-        search = sfpl.Search('Everything Keeps Dissolving')
+        "test book search with one result"
+        search = sfpl.Search("Everything Keeps Dissolving")
         self.assertEqual(len(list(search.getResults())), 1)
 
     def test_list_search(self):
-        search = sfpl.Search('red', _type='list')
+        search = sfpl.Search("red", _type="list")
 
         lists = search.getResults()
 
         for list_ in next(lists):
-            self.assertTrue('red' in list_.title.lower())
+            self.assertTrue("red" in list_.title.lower())
 
     def test_user_search(self):
-        user = sfpl.User('Sublurbanite')
+        user = sfpl.User("Sublurbanite")
 
         user.getFollowers()
         user.getFollowing()
@@ -93,53 +93,55 @@ class TestScraper(unittest.TestCase):
 
     def test_user_error(self):
         with self.assertRaises(sfpl.exceptions.NoUserFound):
-            sfpl.User('eopghpeghip')
+            sfpl.User("eopghpeghip")
 
     def test_branch_hours(self):
-        'test branch hours'
-        branch = sfpl.Branch('west portal')
+        "test branch hours"
+        branch = sfpl.Branch("west portal")
         actual_hours = branch.getHours()
-        for day in ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']:
+        for day in ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]:
             self.assertIn(day, actual_hours)
-            self.assertRegex(actual_hours[day], r'\d+ - \d+')
+            self.assertRegex(actual_hours[day], r"\d+ - \d+")
 
     def test_branch_hours_all(self):
-        'test branch hours on all branches approximately'
+        "test branch hours on all branches approximately"
         for branch_name in sfpl.Branch.BRANCHES:
             branch = sfpl.Branch(branch_name)
             actual_hours = branch.getHours()
-            expected_hours = '12 - 6' if branch_name == 'main library' else '1 - 5'
-            err_msg = f'Sun hours were incorrect for {branch_name}'
-            self.assertEqual(actual_hours['Sun'], expected_hours, err_msg)
+            expected_hours = "12 - 6" if branch_name == "main library" else "1 - 5"
+            err_msg = f"Sun hours were incorrect for {branch_name}"
+            self.assertEqual(actual_hours["Sun"], expected_hours, err_msg)
 
     def test_branch_error(self):
         with self.assertRaises(sfpl.exceptions.NoBranchFound):
-            sfpl.Branch('eighhegiohi;eg')
+            sfpl.Branch("eighhegiohi;eg")
 
     def test_account_error(self):
         with self.assertRaises(sfpl.exceptions.LoginError):
-            sfpl.Account('flbknnklvd', 'uhoegwohi')
+            sfpl.Account("flbknnklvd", "uhoegwohi")
 
     def test_advanced_search(self):
         search = sfpl.AdvancedSearch(
-            includeauthor='J. K. Rowling', excludekeyword='Harry Potter')
+            includeauthor="J. K. Rowling", excludekeyword="Harry Potter"
+        )
 
         results = search.getResults()
 
         for result in next(results):
-            self.assertTrue('harry potter' not in result.title.lower())
+            self.assertTrue("harry potter" not in result.title.lower())
 
     def test_advanced_search_with_zero_results(self):
-        'test advanced search with zero results'
-        search = sfpl.AdvancedSearch(includeauthor='asdafa_does_not_exist')
+        "test advanced search with zero results"
+        search = sfpl.AdvancedSearch(includeauthor="asdafa_does_not_exist")
         self.assertRaises(RuntimeError, lambda: next(search.getResults()))
 
     def test_advanced_search_error(self):
         with self.assertRaises(sfpl.exceptions.MissingFilterTerm):
-            sfpl.AdvancedSearch(soemthingkeyword='Harry Potter')
+            sfpl.AdvancedSearch(soemthingkeyword="Harry Potter")
 
         with self.assertRaises(sfpl.exceptions.MissingFilterTerm):
-            sfpl.AdvancedSearch(excludesomething='Harry Potter')
+            sfpl.AdvancedSearch(excludesomething="Harry Potter")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main(verbosity=2)
